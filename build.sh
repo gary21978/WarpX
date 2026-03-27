bd3cf3b8cdc3e09b9c9dae19a27a38f0f16acddb
gary21978 <gary21978@126.com>
2026-03-27 16:37:01 +0800

    remove .git

--- build.sh
+++ build.sh
@@ -1,1 +1,6 @@
-cmake --fresh -S . -B build -DWarpX_amrex_src=$PWD/amrex -DWarpX_picsar_src=$PWD/picsar -DWarpX_COMPUTE=CUDA -DWarpX_OPENPMD=OFF
+cmake --fresh -S . -B build \
+-DWarpX_amrex_src=$PWD/amrex \
+-DWarpX_picsar_src=$PWD/picsar \
+-DWarpX_COMPUTE=CUDA \
+-DWarpX_OPENPMD=OFF \
+-DWarpX_FFT=ON
